7d927f9a7c7198632f9ede457a1088875d11d629
it猛男阿杰 <itmengnanajie@itmengnanajiedeMacBook-Air.local>
2025-04-09 23:14:30 +0800

    feat: 定义接口

--- src/controllers/woot/index.js
+++ src/controllers/woot/index.js
@@ -1,8 +1,8 @@
-const userModel = require("../../models/woot/index");
+const wootModel = require("../../models/woot/index");
 // 获取WOOT申报表
 const getWootTable = async (req, res) => {
   try {
-    const users = await userModel.getWootTableData();
+    const users = await wootModel.getWootTableData();
     res.json(users);
   } catch (err) {
     console.error("Error fetching users:", err);
