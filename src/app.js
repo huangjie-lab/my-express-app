f98a06b1ab12f8f95363af15a7c463dea52bbcd7
it猛男阿杰 <itmengnanajie@itmengnanajiedeMacBook-Air.local>
2025-04-09 21:17:41 +0800

    feat: 修改数据库

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 // 中间件配置
 app.use(express.json()); // 解析 JSON 请求体
 app.use(express.urlencoded({ extended: true })); // 解析表单数据
-app.use(express.static(path.join(__dirname, "../build"))); // 加载前端静态资源
+app.use(express.static(path.join(__dirname, "../../build"))); // 加载前端静态资源
 
 // 路由配置
 const router = require("./routes/users.js");
@@ -22,12 +22,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-
-// 所有其他路由都重定向到index.html，这样前端路由才能正常工作 (todo... 这里匹配所有路径还有点问题)
-// app.use((req, res, next) => {
-//   res.sendFile(path.join(__dirname, "../build", "index.html"));
-//   next();
-// });
 
 // 导出 app 用于测试
 module.exports = app;
